10c409f5a7ae3d9499184d029b7ff936905b582f
jallubhuvan <bhuvanjoy49@gmail.com>
2016-05-27 09:40:46 +0530

    added salam

--- Doctor/src/Namaste.java
+++ Doctor/src/Namaste.java
@@ -4,6 +4,7 @@ public class Namaste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Namaste Doctor");
+			System.out.println("Salam");
 	} 
 
 }
